08308195a4c551c67300f91e963e7f1abb96508f
Naime Medjek <naimemedjek@gmail.com>
2022-06-03 19:59:27 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let myLeads = []
+const inputBtn = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+
+
+inputBtn.addEventListener ("click",function saveLead() {
+    console.log("Button clicked!")
+})
+
