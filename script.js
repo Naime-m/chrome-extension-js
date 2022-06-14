cc62f05b004e2073ef83d9b90e954869dac3f8ce
Naime Medjek <naimemedjek@gmail.com>
2022-06-14 15:27:40 +0200

    deleteButton style and function

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 let myLeads = []
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
 
 inputBtn.addEventListener ("click", function() {
     myLeads.push(inputEl.value)
     renderLeads()
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
 })
 
 function renderLeads() {
